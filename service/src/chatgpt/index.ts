1af2eb41ce935dfe76b0e71dfd5f36389fbc1252
xxxily <974278171@qq.com>
2023-04-02 10:48:09 +0800

    feat：调整内容

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -130,8 +130,8 @@ async function chatReplyProcess(options: RequestOptions) {
     // const friendlyTips = '\n\nPS：应用目前免费提供给大家使用，让更多人能连接到AI的世界。\n服务器小，请轻点使用~\n'
     const friendlyTips = `
 \n\n[温馨提示]\n 当你看到这个的时候，说明账号已经被干报废，站长已经努力过了。
-\n 如果你想继续免费使用，可以参见这份免费列表[Free ChatGPT Site List](https://hello-ai.anzz.top/home/FreeChatGPTSiteList.html) 
-\n\n 如果期待[Hello-AI](https://hello-ai.anzz.top/)的后续服务，请关注一下Hello-AI的[github](https://github.com/xxxily/hello-ai)，或加群沟通。
+\n 想继续免费使用，可以参见这份免费列表[Free ChatGPT Site List](https://hello-ai.anzz.top/home/FreeChatGPTSiteList.html) 
+\n\n 期待[Hello-AI](https://hello-ai.anzz.top/)的后续服务，请关注一下Hello-AI的[github](https://github.com/xxxily/hello-ai)，或加群沟通。
 \n\n 如果你发现免费列表里的网站都报废了，那就先歇歇吧，不要抱怨。
 \n 我不嫌弃你是白嫖怪，你也别一不能用就砸场子。
 \n ❤️ 听话，做彼此的天使。❤️  
