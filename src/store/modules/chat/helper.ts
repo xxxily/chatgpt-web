8b7f73cc70217da2d22ef946801fc7014223a44b
xxxily <974278171@qq.com>
2023-06-10 16:14:39 +0800

    fix: 修复固定uuid的问题

--- src/store/modules/chat/helper.ts
+++ src/store/modules/chat/helper.ts
@@ -3,7 +3,7 @@ import { ss } from '@/utils/storage'
 const LOCAL_NAME = 'chatStorage'
 
 export function defaultState(): Chat.ChatState {
-  const uuid = 1002
+  const uuid = Date.now()
   return {
     active: uuid,
     /* 默认开启这个会消耗大量token，目前先在前端禁用，后面考虑在后端禁用 */
