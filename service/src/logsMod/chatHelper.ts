7847c68e1bcc9d4df9206423954dd91ea6ba2b33
xxxily <974278171@qq.com>
2023-06-10 23:00:43 +0800

    fix: 修复uuid缺失情况

--- service/src/logsMod/chatHelper.ts
+++ service/src/logsMod/chatHelper.ts
@@ -48,7 +48,7 @@ export async function saveChat(data: ChatBody) {
 
   const result = {
     title: (data.title || data.messages[0]?.content || '').slice(0, 260),
-    uuid: data.uuid,
+    uuid: data.uuid || Date.now(),
     ip: data.realIp || data.ip || '',
     apikey: data.key || data.apikey,
     deviceId: data.deviceId || '',
